41dff19156ef5bf3abf4b35726062b26a4cc6e7b
Grégoire <aim-schnapps-coat@duck.com>
2024-10-12 19:48:04 +0200

    tictactoe :)

---
+++ src/lib.rs
@@ -0,0 +1,306 @@
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub struct Grid {
+    pub matrix: [[Player; 3]; 3],
+    pub number_of_turns: i32,
+    pub player_turn: Player,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Player {
+    X,
+    O,
+    Empty,
+}
+
+impl fmt::Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let c = match self {
+            Self::X => 'X',
+            Self::O => 'O',
+            Self::Empty => ' ',
+        };
+
+        write!(f, "{}", c)
+    }
+}
+
+impl Grid {
+    pub fn new() -> Grid {
+        return Grid::from([
+            [Player::Empty, Player::Empty, Player::Empty],
+            [Player::Empty, Player::Empty, Player::Empty],
+            [Player::Empty, Player::Empty, Player::Empty],
+        ]);
+    }
+
+    pub fn from(matrix: [[Player; 3]; 3]) -> Grid {
+        let number_of_turn = Grid::count_number_of_turns(matrix);
+
+        let player_turn = if number_of_turn & 1 == 0 {
+            Player::X
+        } else {
+            Player::O
+        };
+        Grid {
+            matrix,
+            number_of_turns: number_of_turn,
+            player_turn,
+        }
+    }
+
+    fn count_number_of_turns(matrix: [[Player; 3]; 3]) -> i32 {
+        let mut number_of_turn = 0;
+
+        for &row in matrix.iter() {
+            for &square in row.iter() {
+                match square {
+                    Player::Empty => (),
+                    Player::O => number_of_turn += 1,
+                    Player::X => number_of_turn += 1,
+                }
+            }
+        }
+
+        number_of_turn
+    }
+
+    pub fn set(&mut self, x: usize, y: usize) -> Result<(), Player> {
+        if self.matrix[y][x] != Player::Empty {
+            return Err(self.matrix[y][x]);
+        }
+
+        self.matrix[y][x] = self.player_turn;
+        self.number_of_turns += 1;
+        self.player_turn = if self.player_turn == Player::X {
+            Player::O
+        } else {
+            Player::X
+        };
+
+        Ok(())
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.number_of_turns == 9
+    }
+
+    pub fn best_play(&self) -> Option<(usize, usize)> {
+        let mut best_play = None;
+        let mut best_x = None;
+        let mut best_y = None;
+
+        let mut update_best_score = |score, x, y| match best_play {
+            Some(best_score) => {
+                if best_score < score {
+                    best_play = Some(score);
+                    best_x = Some(x);
+                    best_y = Some(y);
+                }
+            }
+            None => {
+                best_play = Some(score);
+                best_x = Some(x);
+                best_y = Some(y);
+            }
+        };
+
+        for (y, row) in self.matrix.iter().enumerate() {
+            for (x, _) in row.iter().enumerate() {
+                let mut g = self.clone();
+
+                match g.set(x, y) {
+                    Ok(_) => {
+                        let score = minmax(g);
+                        update_best_score(score, x, y);
+                    }
+                    Err(_) => (),
+                }
+            }
+        }
+
+        let res = match best_play {
+            Some(_) => Some((best_x.unwrap(), best_y.unwrap())),
+            None => None,
+        };
+
+        return res;
+    }
+
+    pub fn has_winner(&self) -> bool {
+        if self.check_diag() {
+            return true;
+        }
+        for i in 0..3 {
+            if self.check_col(i) {
+                return true;
+            }
+            if self.check_row(i) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn check_col(&self, col: usize) -> bool {
+        if self.matrix[0][col] == Player::Empty {
+            return false;
+        }
+
+        self.matrix[0][col] == self.matrix[1][col] && self.matrix[1][col] == self.matrix[2][col]
+    }
+
+    fn check_row(&self, row: usize) -> bool {
+        if self.matrix[row][0] == Player::Empty {
+            return false;
+        }
+
+        self.matrix[row][0] == self.matrix[row][1] && self.matrix[row][1] == self.matrix[row][2]
+    }
+
+    fn check_diag(&self) -> bool {
+        if self.matrix[0][0] == self.matrix[1][1]
+            && self.matrix[1][1] == self.matrix[2][2]
+            && self.matrix[0][0] != Player::Empty
+        {
+            return true;
+        }
+
+        if self.matrix[2][0] == self.matrix[1][1]
+            && self.matrix[1][1] == self.matrix[0][2]
+            && self.matrix[0][2] != Player::Empty
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+pub fn minmax(grid: Grid) -> i32 {
+    if grid.has_winner() {
+        return 1;
+    }
+
+    if grid.is_full() {
+        return 0;
+    }
+
+    let mut score = 0;
+    for (y, &row) in grid.matrix.iter().enumerate() {
+        for (x, &square) in row.iter().enumerate() {
+            if square == Player::Empty {
+                let mut ng = grid.clone();
+                let _ = ng.set(x, y);
+                score -= minmax(ng);
+            }
+        }
+    }
+
+    return score;
+}
+
+pub fn display(grid: &Grid) {
+    let m = grid.matrix;
+    println!("   1   2   3");
+    println!("a  {} | {} | {} ", m[0][0], m[0][1], m[0][2]);
+    println!("  -----------");
+    println!("b  {} | {} | {} ", m[1][0], m[1][1], m[1][2]);
+    println!("  -----------");
+    println!("c  {} | {} | {} ", m[2][0], m[2][1], m[2][2]);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_diag() {
+        let grid = Grid::from([
+            [Player::X, Player::Empty, Player::Empty],
+            [Player::O, Player::X, Player::Empty],
+            [Player::O, Player::Empty, Player::X],
+        ]);
+
+        assert_eq!(grid.has_winner(), true);
+    }
+
+    #[test]
+    fn check_antidiag() {
+        let grid = Grid::from([
+            [Player::O, Player::Empty, Player::X],
+            [Player::X, Player::X, Player::Empty],
+            [Player::X, Player::O, Player::O],
+        ]);
+
+        assert_eq!(grid.has_winner(), true);
+    }
+
+    #[test]
+    fn check_col() {
+        let grid = Grid::from([
+            [Player::O, Player::X, Player::Empty],
+            [Player::O, Player::X, Player::X],
+            [Player::O, Player::Empty, Player::X],
+        ]);
+
+        assert_eq!(grid.has_winner(), true);
+    }
+
+    #[test]
+    fn check_row() {
+        let grid = Grid::from([
+            [Player::X, Player::X, Player::X],
+            [Player::O, Player::X, Player::Empty],
+            [Player::O, Player::Empty, Player::O],
+        ]);
+
+        assert_eq!(grid.has_winner(), true);
+    }
+
+    #[test]
+    fn no_winner() {
+        let grid = Grid::from([
+            [Player::X, Player::Empty, Player::Empty],
+            [Player::O, Player::O, Player::X],
+            [Player::O, Player::X, Player::X],
+        ]);
+
+        assert_eq!(grid.has_winner(), false);
+    }
+
+    #[test]
+    fn empty_no_winner() {
+        let grid = Grid::from([
+            [Player::Empty, Player::Empty, Player::Empty],
+            [Player::Empty, Player::Empty, Player::Empty],
+            [Player::Empty, Player::Empty, Player::Empty],
+        ]);
+
+        assert_eq!(grid.has_winner(), false);
+    }
+
+    #[test]
+    fn is_full() {
+        let grid = Grid::from([
+            [Player::X, Player::X, Player::O],
+            [Player::O, Player::O, Player::X],
+            [Player::O, Player::X, Player::X],
+        ]);
+
+        assert_eq!(grid.is_full(), true);
+    }
+
+    #[test]
+    fn is_not_full() {
+        let grid = Grid::from([
+            [Player::Empty, Player::X, Player::O],
+            [Player::O, Player::Empty, Player::X],
+            [Player::O, Player::X, Player::X],
+        ]);
+
+        assert_eq!(grid.is_full(), false);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,109 @@
+use std::io;
+use tictactoe::*;
+
+fn main() {
+    let grid = Grid::new();
+    main_loop(grid);
+}
+
+fn main_loop(mut grid: Grid) {
+    while !grid.is_full() && !grid.has_winner() {
+        let mut x;
+        let mut y;
+        if grid.player_turn == Player::X {
+            display(&grid);
+            loop {
+                (x, y) = player_turn();
+                match grid.set(x as usize, y as usize) {
+                    Ok(_) => break,
+                    Err(p) => println!("Theses coordinates already have an {p}. Please enter again : "),
+                }
+            }
+        } else {
+            match grid.best_play() {
+                Some(coo) => { let _ = grid.set(coo.0, coo.1); },
+                None => { break; },
+            }
+        }
+    }
+    end_game(grid);
+}
+
+fn end_game(grid: Grid) {
+    let mut winner = Player::X;
+    if winner == grid.player_turn {
+        winner = Player::O;
+    }
+    display(&grid);
+    if !grid.is_full() {
+        println!("Player {} won!", winner);
+    } else {
+        println!("Draw :(");
+    }
+}
+
+fn player_turn() -> (i32, i32) {
+    loop {
+        println!("Please enter some coordinates : ");
+        match read_player_trial() {
+            Ok(res) => {
+                return res;
+            },
+            Err(e) => println!("{e} : "),
+        }
+    }
+}
+
+fn read_player_trial() -> Result<(i32, i32), &'static str> {
+    let mut trial = String::new();
+
+    io::stdin()
+        .read_line(&mut trial)
+        .expect("Failed to read line");
+
+    let trimmed = trial.trim();
+    let mut x = None;
+    let mut y = None;
+
+    if trimmed.chars().count() != 2 {
+        return Err("Please enter valid coordinates");
+    }
+
+    for (i, _) in trimmed.char_indices() {
+        let char = &trimmed[i..=i];
+
+
+        match char.parse::<i32>() {
+            Ok(n) => match x {
+                None => x = {
+                    if n < 4 && n > 0 {
+                        Some(n - 1)
+                    } else {
+                        return Err("Please enter valid coordinates");
+                    }
+                },
+                Some(_) => {
+                    return Err("Please enter valid coordinates");
+                }
+            },
+            Err(_) => match y {
+                None => {
+                    if char.eq("a") {
+                        y = Some(0);
+                    } else if char.eq("b") {
+                        y = Some(1);
+                    } else if char.eq("c") {
+                        y = Some(2);
+                    } else {
+                        return Err("Please enter valid coordinates");
+                    }
+                }
+                Some(_) => {
+                    return Err("Please enter valid coordinates");
+                }
+            },
+        }
+    }
+
+    Ok((x.unwrap(), y.unwrap()))
+}
